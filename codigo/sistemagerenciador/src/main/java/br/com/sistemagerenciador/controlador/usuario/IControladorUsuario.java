eea72b25e2698a13d5f213c8ede2ecc9d5d64466
jean <jean.varela1@gmail.com>
2017-08-13 16:02:51 -0300

    Removido o projeto auxiliar

--- codigo/sistemagerenciador/src/main/java/br/com/sistemagerenciador/controlador/usuario/IControladorUsuario.java
+++
@@ -1,8 +0,0 @@
-package br.com.sistemagerenciador.controlador.usuario;
-
-import br.com.sistemagerenciador.modelo.entidade.usuario.Usuario;
-
-public interface IControladorUsuario {
-
-	public void adcionarUsuario(Usuario usuario);
-}
